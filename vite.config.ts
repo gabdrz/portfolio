601e540e55437953dc3edfc8b9fc8f8fccfe47d5
gabdrz <gabdrz.00@gmail.com>
2025-01-01 21:04:36 -0500

    fix: deployment updates and adjusted project scrolling

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
   server: {
-    host: true, // This exposes the server to your local network
-    port: 5173, // Default Vite port
+    port: 3000,
+    open: true
   }
-})
+});
