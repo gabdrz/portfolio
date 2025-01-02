80e43fd43e08a70b6aad559bd2045ec1b1ac6221
gabdrz <gabdrz.00@gmail.com>
2025-01-01 22:18:28 -0500

    feat: Added favicon

--- src/data/projects/index.ts
+++ src/data/projects/index.ts
@@ -3,14 +3,11 @@ import { Project } from './types';
 
 // UX Design Projects
 import { rocketLeagueSideswipe } from './uxDesign/rocketLeagueSideswipe';
-import { doctorsAppointment } from './uxDesign/doctorsAppointment'; 
-import { seatReservationWeb } from './uxDesign/seatReservationWeb';
-import { seatReservationApp } from './uxDesign/seatReservationApp';
 
 // Software Dev Projects
 import { portfolioWebsite } from './softwareDev/portfolioWebsite';
-import { carMaintenance } from './softwareDev/carMaintenance';
-import { freedomBoard } from './softwareDev/freedomBoard';
+
+import { workInProgress } from './workInProgress';
 
 export interface ProjectsData {
  uxDesign: Project[];
@@ -20,13 +17,10 @@ export interface ProjectsData {
 export const projectsData: ProjectsData = {
  uxDesign: [
    rocketLeagueSideswipe,
-   doctorsAppointment,
-   seatReservationWeb,
-   seatReservationApp
+   workInProgress
  ],
  softwareDev: [
    portfolioWebsite,
-   carMaintenance, 
-   freedomBoard
+   workInProgress
  ]
 };
